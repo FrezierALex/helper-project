311b694641353286053b8741bfb3add5a84b9a2c
FrezierALex <obamaalex999@gmail.com>
2026-01-06 15:56:45 +0300

    Update config.py with hardcoded Discord credentials
    Replaced environment variable retrieval with hardcoded values for Discord webhook URL, bot token, and command channel ID.
    
    Signed-off-by: FrezierALex <obamaalex999@gmail.com>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL')
-DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
-COMMAND_CHANNEL_ID = os.environ.get('COMMAND_CHANNEL_ID')
+DISCORD_WEBHOOK_URL = os.environ.get('https://discord.com/api/webhooks/1457793195026026498/uVEOslH5OJaIHbTff8tUjLWei4M-Vqydip2XNRUfrC7zu1htJDnOqcngYb_0ltZearFd')
+DISCORD_BOT_TOKEN = os.environ.get('MTQ1Nzc4NDIxODcwMzc2MTUwMQ.G9o1t9.YZYyuDKXwFqwffw7sInQA6wWISJ7gK69aBLhDM')
+COMMAND_CHANNEL_ID = os.environ.get('1457801276006993971')
