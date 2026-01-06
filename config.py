dd1904a3162d6cbd353946b1e4f10253bd72e432
FrezierALex <obamaalex999@gmail.com>
2026-01-06 15:58:59 +0300

    Update config.py
    Signed-off-by: FrezierALex <obamaalex999@gmail.com>

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-import os
 
-DISCORD_WEBHOOK_URL = os.environ.get('https://discord.com/api/webhooks/1457793195026026498/uVEOslH5OJaIHbTff8tUjLWei4M-Vqydip2XNRUfrC7zu1htJDnOqcngYb_0ltZearFd')
-DISCORD_BOT_TOKEN = os.environ.get('MTQ1Nzc4NDIxODcwMzc2MTUwMQ.G9o1t9.YZYyuDKXwFqwffw7sInQA6wWISJ7gK69aBLhDM')
-COMMAND_CHANNEL_ID = os.environ.get('1457801276006993971')
